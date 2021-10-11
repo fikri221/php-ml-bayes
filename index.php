0a520a1b36079b020a294d0d63b2fa9989c4dd0b
Fikri Lazuardi <fikrim69@gmail.com>
2021-10-11 21:58:50 +0700

    landing page

--- index.php
+++ index.php
@@ -1,127 +1,268 @@
-<?php
-require_once './vendor/autoload.php';
-include_once("config.php");
+<!DOCTYPE html>
+<html lang="en">
 
-use Phpml\Classification\NaiveBayes;
-use Phpml\Classification\KNearestNeighbors;
-use Phpml\Classification\SVC;
-use Phpml\Dataset\CsvDataset;
-use Phpml\SupportVectorMachine\Kernel;
-use Phpml\Dataset\Demo\IrisDataset;
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-?>
+    <title>Medilab Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-<!-- Page Content -->
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12 text-center">
-      <h1 class="mt-5">PHP-ML Demo (Classification)</h1>
-      <p class="lead">&nbsp;</p>
-      <form action="" method="post" enctype="multipart/form-data">
-        <fieldset style="background-color: #dedede">
-          <legend>Training</legend>
-          <p>Dataset<br />
-            <input type="radio" name="dataset" value="iris" checked="checked" />Iris Dataset
-          </p>
-          <p>Algorithm:
-            <input type="radio" name="algo" value="bayes" checked="checked" /> Naive Bayes
-            <input type="radio" name="algo" value="knn" /> kNN
-            <input type="radio" name="algo" value="svc" /> SVC
-          </p>
-        </fieldset>
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-        <fieldset style="background-color: #dedede">
-          <legend>Testing Data</legend>
-          <p>
-            Usia :
-            <select name="usia">
-              <option selected="selected">--Pilih--</option>
-              <option>ya</option>
-              <option>tidak</option>
-            </select>
-          </p>
-          <p>
-            Jumlah Anak :
-            <select name="jml_anak">
-              <option selected="selected">--Pilih--</option>
-              <option>ya</option>
-              <option>tidak</option>
-            </select>
-          </p>
-          <p>
-            Tekanan Darah :
-            <select name="tekanan_darah">
-              <option selected="selected">--Pilih--</option>
-              <option>ya</option>
-              <option>tidak</option>
-            </select>
-          </p>
-          <p>
-            Riwayat Penyakit :
-            <select name="riwayat_penyakit">
-              <option selected="selected">--Pilih--</option>
-              <option>ya</option>
-              <option>tidak</option>
-            </select>
-          </p>
-        </fieldset>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-        <p class="lead"></p>
-        <p class="lead">
-          <input type="submit" name="proses" value="Process..." />
-        </p>
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-      </form>
-      <?php
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
 
-      if (isset($_POST['proses'])) {
-        $algo = $_POST['algo'];
+    <!-- =======================================================
+  * Template Name: Medilab - v4.6.0
+  * Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
 
-        $dataset = new IrisDataset();
+<body>
 
-        $row = 1;
-        /*
-  data ada 4 parameter dan 1 label
-  sepal length,sepal width,petal length,petal width
-  label = jenis iris
-  */
-        $samples = $dataset->getSamples();
-        $labels = $dataset->getTargets();
-        //echo "<pre>"; print_r($samples); echo "</pre>";
-        //echo "<pre>"; print_r($labels); echo "</pre>"; exit;
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed">
+        <div class="container d-flex align-items-center">
 
-        //learning process
-        $dtesting = array();
-        // foreach ($_POST['parameter'] as $value) {
-        //   $dtesting[] = $value;
-        // }
-        $dtesting[] = $_POST['usia'];
-        $dtesting[] = $_POST['jml_anak'];
-        $dtesting[] = $_POST['tekanan_darah'];
-        $dtesting[] = $_POST['riwayat_penyakit'];
+            <h1 class="logo me-auto"><a href="index.html">Contralab</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-        $class_hasil = "unknown";
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
 
-        if ($algo == 'bayes') {
-          $classifier = new NaiveBayes();
-          //train every labels
-          $classifier->train($samples, $labels);
-          $class_hasil = $classifier->predict($dtesting);
-        } else if ($algo == 'knn') {
-          $classifier = new KNearestNeighbors();
-          $classifier->train($samples, $labels);
-          $class_hasil = $classifier->predict($dtesting);
-        } else if ($algo == 'svc') {
-          $classifier = new SVC(Kernel::LINEAR, $cost = 1000);
-          $classifier->train($samples, $labels);
-          $class_hasil = $classifier->predict($dtesting);
-        }
+            <a href="#appointment" class="appointment-btn scrollto"><span>Login</span></a>
 
-        //tampilkan
-        echo "<h2 class='lead'>The Results : $class_hasil</h2>";
-        echo "<img src='img/$class_hasil.png' width='150'/>";
-      }
-      ?>
-    </div>
-  </div>
-</div>
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+        <div class="container">
+            <h1>Welcome to Contralab</h1>
+            <h2>Contralab adalah sebuah website yang dapat mengklasifikasikan<br>
+                alat kontrasepsi yang cocok berdasarkan riwayat kesehatan Anda.</h2>
+            <a href="ml-bayes.php" class="btn-get-started scrollto">Mulai Analisa</a>
+        </div>
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= Why Us Section ======= -->
+        <section id="why-us" class="why-us">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-4 d-flex align-items-stretch">
+                        <div class="content">
+                            <h3>Why Choose Contralab?</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+                            </p>
+                            <div class="text-center">
+                                <a href="#" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 d-flex align-items-stretch">
+                        <div class="icon-boxes d-flex flex-column justify-content-center">
+                            <div class="row">
+                                <div class="col-xl-4 d-flex align-items-stretch">
+                                    <div class="icon-box mt-4 mt-xl-0">
+                                        <i class="bx bx-cube-alt"></i>
+                                        <h4>Ullamco laboris ladore pan</h4>
+                                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                                    </div>
+                                </div>
+                                <div class="col-xl-4 d-flex align-items-stretch">
+                                    <div class="icon-box mt-4 mt-xl-0">
+                                        <i class="bx bx-images"></i>
+                                        <h4>Labore consequatur</h4>
+                                        <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- End .content-->
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End Why Us Section -->
+
+        <!-- ======= Services Section ======= -->
+        <section id="services" class="services">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>Alat Kontrasepsi</h2>
+                    <p>Berikut beberapa alat kontrasepsi yang nantinya digunakan sebagai data label atau class.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-syringe"></i></div>
+                            <h4><a href="">Suntik</a></h4>
+                            <p>Suntik KB adalah kontrasepsi hormonal yang mengandung hormon progestogen (progestin). 
+                                Hormon ini serupa dengan hormon alami wanita, yaitu progesteron, dan dapat menghentikan ovulasi.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-pills"></i></div>
+                            <h4><a href="">Pil</a></h4>
+                            <p>Pil KB adalah kelompok obat yang digunakan untuk mencegah kehamilan. 
+                                Ada dua jenis pil KB, yaitu pil KB kombinasi dan pil KB khusus progestin.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fab fa-yandex-international"></i></div>
+                            <h4><a href="">IUD</a></h4>
+                            <p>IUD yang merupakan singkatan dari intrauterine device (alat kontrasepsi dalam rahim), 
+                                juga dikenal dengan sebutan kontrasepsi spiral. IUD bekerja dengan cara menghambat 
+                                gerakan sperma menuju saluran rahim untuk mencegah pembuahan, sehingga tidak terjadi kehamilan.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-download"></i></div>
+                            <h4><a href="">Implan</a></h4>
+                            <p>KB Implan merupakan salah satu alat kontrasepsi yang berbentuk seperti tabung plastik elastis dan berukuran kecil menyerupai 
+                                batang korek api yang dimasukkan ke jaringan lemak pada lengan atas wanita.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-space-shuttle"></i></div>
+                            <h4><a href="">Kondom</a></h4>
+                            <p>Kondom merupakan alat penghalang fisik yang menghentikan sperma 
+                                memasuki vagina dan mencapai sel telur. Penggunaan kondom secara konsisten dan benar dapat mengurangi 
+                                risiko penularan penyakit menular seksual seperti herpes genital dan sifilis.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-cut"></i></div>
+                            <h4><a href="">Vasektomi</a></h4>
+                            <p>Vasektomi adalah prosedur kontrasepsi pada pria yang dilakukan dengan cara memutus penyaluran sperma ke air mani. 
+                                Dengan demikian, air mani tidak akan mengandung sperma, sehingga kehamilan dapat dicegah.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                        <div class="icon-box">
+                            <div class="icon"><i class="fas fa-ban"></i></div>
+                            <h4><a href="">Tubektomi</a></h4>
+                            <p>Tubektomi adalah prosedur pemotongan atau penutupan tuba falopi atau saluran indung telur yang menghubungkan 
+                                ovarium ke rahim. Setelah tubektomi, sel-sel telur tidak akan bisa memasuki rahim sehingga tidak dapat 
+                                dibuahi. Prosedur ini juga akan menghalangi sperma ke tuba falopi.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section><!-- End Services Section -->
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Contralab</h3>
+                        <p>
+                            A108 Adam Street <br>
+                            New York, NY 535022<br>
+                            United States <br><br>
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Services</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-newsletter">
+                        <h4>Join Our Newsletter</h4>
+                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/purecounter/purecounter.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
